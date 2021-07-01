d578ee3448b1d6642694ec3e271f8bcce5f59e4b
Niema Moshiri <niemamoshiri@gmail.com>
2021-07-01 07:27:14 -0700

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import argparse
+
+# parse user args
+def parse_args():
+    pass # TODO
+
+# main execution
+if __name__ == "__main__":
+    # parse and check user args
+    # TODO
